ead05dfd968b8edfb2009f3d4c27373f9a8ff9c2
whatsnext <whatsnext@localhost.localdomain>
2025-12-27 00:44:48 +0530

    terraform:v3

--- userdata.sh
+++ userdata.sh
@@ -8,4 +8,4 @@ systemctl enable apache2
 systemctl start apache2
 
 
-echo "<h1>Hello from Apache on $(hostname) powerd by abishek veeramala</h1>" > /var/www/html/index.html
+echo "<h1>Hello from Apache on $(hostname)</h1>" > /var/www/html/index.html
